a0497a6dd53cfcf158f5d2882d901aae4e43e2fe
UrosKuzmanovic <ukuzmanovic1@gmail.com>
2023-08-16 22:49:51 +0200

    Added build.sh, updated README and Makefile

--- Makefile
+++ Makefile
@@ -11,16 +11,16 @@ help:
 	@echo 'make shell [container]		- enter PHP-fpm container'
 
 up:
-	@docker-compose -f docker-compose.yml up -d --build --remove-orphans
+	@docker-compose -f docker-compose.yml up -d --build --remove-orphans $(if $(container),$(container),)
 
 down:
-	@docker-compose -f docker-compose.yml down
+	@docker-compose -f docker-compose.yml down $(if $(container),$(container),)
 
 start:
-	@docker-compose -f docker-compose.yml start
+	@docker-compose -f docker-compose.yml start $(if $(container),$(container),)
 
 stop:
-	@docker-compose -f docker-compose.yml stop
+	@docker-compose -f docker-compose.yml stop $(if $(container),$(container),)
 
 shell:
 	@docker exec -it $$(docker ps | grep 'foodnet-docker_$(if $(container),$(container),$(DEFAULT_CONTAINER))' | awk '{ print $$1 }') bash
---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+source .env
+
+git_username="UrosKuzmanovic"
+git_password="Ukikuzma1996!"
+
+microservices=(
+  "authenticator"
+  "email"
+  "foodNet-backend"
+)
+
+for microservice in "${microservices[@]}"; do
+  cd "microservices"
+
+  if [ -d "$microservice" ]; then
+    echo "Repository $microservice exists, updating with git pull"
+    cd "$microservice"
+    git pull
+  else
+    echo "Cloning repository: $microservice"
+    git clone "https://$GIT_USERNAME:$GIT_PASSWORD@github.com/$git_username/$microservice.git"
+    cd "$microservice"
+  fi
+
+  cd "../.."
+
+  echo "Building container: $microservice"
+
+  if [ "$microservice" = "foodNet-backend" ]; then
+    container="foodnet"
+  else
+    container=$microservice
+  fi
+
+  make stop "container=$container"
+  make up "container=$container"
+
+  sudo chmod -R 777 "microservices/$microservice"
+
+  container_name="foodnet-docker_"$container"_1"
+
+  if [ -d vendor ]; then
+    docker exec -it "$container_name" rm -rf vendor
+  fi
+
+  if [ -d composer.lock ]; then
+    docker exec -it "$container_name" rm composer.lock
+  fi
+
+  docker exec -it "$container_name" sh -c "yes | composer install"
+
+  docker exec -it "$container_name" sh -c "php bin/console doctrine:database:create"
+
+  docker exec -it "$container_name" sh -c "yes | php bin/console doctrine:migrations:migrate"
+
+  docker exec -it "$container_name" sh -c "exit"
+
+done
+
+make start
+
+echo "Build completed!"
